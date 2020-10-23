874255804f78b68288cc8d55772b1bb626c6454e
lkovacic <leonelkovacic@gmail.com>
2020-10-22 23:42:47 -0300

    Added unit test to all reducers

--- angular-wishlist/src/app/models/destinos-viajes-state.model.spec.ts
+++ angular-wishlist/src/app/models/destinos-viajes-state.model.spec.ts
@@ -3,7 +3,10 @@ import {
     DestinosViajesState,
     initializeDestinosViajesState,
     InitMyDataAction,
-    NuevoDestinoAction
+    NuevoDestinoAction,
+    DestinoBorrarAction,
+    VoteUpAction,
+    VoteDownAction
   } from './destinos-viajes-state.model';
   import { DestinoViaje } from './destino-viaje.model';
   
@@ -22,6 +25,49 @@ import {
       const newState: DestinosViajesState = reducerDestinosViajes(prevState, action);
       expect(newState.items.length).toEqual(1);
       expect(newState.items[0].nombre).toEqual('barcelona');
+    });
+    it('should reduce new item added and delete it', () => {
+      const prevState: DestinosViajesState = initializeDestinosViajesState();
+      const createAction: NuevoDestinoAction = new NuevoDestinoAction(new DestinoViaje('barcelona', 'url'));
+      const createState: DestinosViajesState = reducerDestinosViajes(prevState, createAction);
+      expect(createState.items.length).toEqual(1);
+
+      const deleteAction: DestinoBorrarAction = new DestinoBorrarAction(new DestinoViaje('barcelona', 'url'));
+      const deleteState: DestinosViajesState = reducerDestinosViajes(prevState, deleteAction);
+      expect(deleteState.items.length).toEqual(0);
+    });
+    it('should reduce new item added and mark it as favorite', () => {
+      const prevState: DestinosViajesState = initializeDestinosViajesState();
+      const createAction: NuevoDestinoAction = new NuevoDestinoAction(new DestinoViaje('barcelona', 'url'));
+      const createState: DestinosViajesState = reducerDestinosViajes(prevState, createAction);
+      expect(createState.items.length).toEqual(1);
+      expect(createState.items[0].isSelected).toBeTruthy();
+    });
+    it('should reduce new item added vote up', () => {
+      const destinoViaje: DestinoViaje = new DestinoViaje('barcelona', 'url');
+      const prevState: DestinosViajesState = initializeDestinosViajesState();
+      const createAction: NuevoDestinoAction = new NuevoDestinoAction(destinoViaje);
+      const createState: DestinosViajesState = reducerDestinosViajes(prevState, createAction);
+      expect(createState.items.length).toEqual(1);
+      expect(createState.items[0].votes).toEqual(0);
+
+      const voteUpAction: VoteUpAction = new VoteUpAction(destinoViaje);
+      const voteUpState: DestinosViajesState = reducerDestinosViajes(createState, voteUpAction);
+      expect(voteUpState.items.length).toEqual(1);
+      expect(voteUpState.items[0].votes).toEqual(1);
+    });
+    it('should reduce new item added vote down', () => {
+      const destinoViaje: DestinoViaje = new DestinoViaje('barcelona', 'url');
+      const prevState: DestinosViajesState = initializeDestinosViajesState();
+      const createAction: NuevoDestinoAction = new NuevoDestinoAction(destinoViaje);
+      const createState: DestinosViajesState = reducerDestinosViajes(prevState, createAction);
+      expect(createState.items.length).toEqual(1);
+      expect(createState.items[0].votes).toEqual(0);
+
+      const voteDownAction: VoteDownAction = new VoteDownAction(destinoViaje);
+      const voteDownState: DestinosViajesState = reducerDestinosViajes(createState, voteDownAction);
+      expect(voteDownState.items.length).toEqual(1);
+      expect(voteDownState.items[0].votes).toEqual(-1);
     });
   });
   
